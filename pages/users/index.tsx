4ed952baa2b614185cdb5f189f9f7fec546798af
Irfan Saputra <irfansaputra1@gmail.com>
2021-09-13 12:22:51 +0700

    getStaticProps

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,9 +1,37 @@
+import { useRouter } from 'next/router';
 import Layout from '../../components/Layout';
+import styles from '../../styles/Users.module.css';
 
-export default function index() {
+interface UsersProps{
+  dataUsers : Array<any>;
+}
+
+export default function Users(props : UsersProps) {
+  const { dataUsers } = props;
+  const router = useRouter();
+
   return (
     <Layout pageTitle="Users Page">
-      <p>User Pages</p>
+      {dataUsers.map((user) => (
+      <div className={styles.card} key={user.id} onClick={() => router.push(`/users/${user.id}`)}>
+      <p>
+{user.name}
+      </p>
+      <p>
+{user.email}
+      </p>
+      </div>
+      ))}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const dataUsers = await res.json();
+  return {
+    props: {
+      dataUsers,
+    },
+  };
 }
