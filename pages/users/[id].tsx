4bc4cef8878b76049ac41b0ebe6f751c2f930efa
Irfan Saputra <irfansaputra1@gmail.com>
2021-09-13 12:24:10 +0700

    add getStaticPaths

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,17 +1,61 @@
-import { useRouter } from 'next/router';
 import Layout from '../../components/Layout';
 
-export default function Details() {
-  const router = useRouter();
-  const { id } = router.query;
+interface User {
+  id:number;
+  name:string;
+  email:string;
+  phone:string;
+  website:string;
+}
+
+interface UserDetailProps{
+  user : User;
+}
+
+export default function Details(props : UserDetailProps) {
+  const { user } = props;
+
   return (
     <Layout pageTitle="Details">
-      <p>
-Detail user
-{'  '}
-{id}
-{'  '}
-      </p>
+      <p>{user.name}</p>
+      <p>{user.email}</p>
+      <p>{user.phone}</p>
+      <p>{user.website}</p>
     </Layout>
   );
+}
+
+export async function getStaticPaths() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const dataUsers = await res.json();
+
+  const paths = dataUsers.map((user:User) => ({
+    params: {
+      id: `${user.id}`,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+interface GetStaticProps {
+  params:{
+    id:string;
+  }
+}
+
+// untuk mendapatkan detail usernya
+export async function getStaticProps(context: GetStaticProps) {
+  const { id } = context.params;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const user = await res.json();
+
+  return {
+    props: {
+      user,
+    },
+  };
 }
