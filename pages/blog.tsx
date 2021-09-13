40ae409c205aaaf434a76d1f27a80b3cdfc2f45c
Irfan Saputra <irfansaputra1@gmail.com>
2021-09-13 12:25:36 +0700

    add getServerSideProps

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,10 +1,38 @@
 import Layout from '../components/Layout';
+import styles from '../styles/Blogs.module.css';
 
-/* eslint-disable react/react-in-jsx-scope */
-export default function blog() {
+interface Post{
+  id: number;
+  userId:number;
+  title: string;
+  body:string;
+}
+
+interface BlogProps{
+  dataBlogs : Post[];
+}
+
+export default function blog(props:BlogProps) {
+  const { dataBlogs } = props;
   return (
    <Layout pageTitle="Blog Pages">
-    <p>Blog pages</p>
+    {dataBlogs.map((datablog) => (
+        <div key={datablog.id} className={styles.card}>
+        <h3>{datablog.title}</h3>
+        <p>{datablog.body}</p>
+        </div>
+    ))}
    </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const dataBlogs = await res.json();
+
+  return {
+    props: {
+      dataBlogs,
+    },
+  };
 }
